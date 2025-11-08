3d78bc8477b6918f47b24cfed3abce30e6c0ee0f
Vitor Carvalho <vitormarcarvalho@gmail.com>
2025-11-08 11:53:36 -0300

    Criando projeto em sql

--- src/main/resources/db/migration/V002__inserts_inicial.sql
+++ src/main/resources/db/migration/V002__inserts_inicial.sql
@@ -1,24 +1,38 @@
-insert into TB_TERMO_ACEITE (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, DESCRICAO)
-VALUES (CURRENT_TIMESTAMP, null,
-        'Eu entendo e concordo que as informações fornecidas podem ser transferidas para terceiros, incluindo prestadores de serviços, afiliados e parceiros comerciais da empresa Gastro Reserva, conforme necessário para cumprir as finalidades descritas neste termo. ');
+-- ========================================================
+-- V2: SCRIPT DE CARGA DE DADOS PARA MICROSOFT SQL SERVER
+-- ========================================================
 
-insert into TB_USUARIO (DTYPE, DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, TIPO_USUARIO, CPF, NOME, SENHA, EMAIL, ENDERECO,
+-- Carga na tabela tb_termo_aceite
+INSERT INTO TB_TERMO_ACEITE (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, DESCRICAO)
+VALUES (GETDATE(), NULL,
+        'Eu entendo e concordo que as informações fornecidas podem ser transferidas para terceiros, incluindo prestadores de serviços, afiliados e parceiros comerciais da empresa Gastro Reserva, conforme necessário para cumprir as finalidades descritas neste termo.');
+GO
+
+-- Carga na tabela tb_usuario
+INSERT INTO TB_USUARIO (DTYPE, DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, TIPO_USUARIO, CPF, NOME, SENHA, EMAIL, ENDERECO,
                         TERMO_ACEITE_COD)
-VALUES (null, CURRENT_TIMESTAMP, null, 'FUNCIONARIO', '93029719022', 'nomeTeste', 'teste', 'teste@gmail.com',
+VALUES (NULL, GETDATE(), NULL, 'FUNCIONARIO', '93029719022', 'nomeTeste', 'teste', 'teste@gmail.com',
         'qualquer endereço', 1);
+GO
 
-insert into TB_RESTAURANTE (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, NOME_RESTAURANTE, ENDERECO)
-values (CURRENT_TIMESTAMP, NULL, 'Sabor do Sertão', 'Avenida Brasil, 456, Rio de Janeiro - RJ');
+-- Carga na tabela tb_restaurante
+INSERT INTO TB_RESTAURANTE (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, NOME_RESTAURANTE, ENDERECO)
+VALUES (GETDATE(), NULL, 'Sabor do Sertão', 'Avenida Brasil, 456, Rio de Janeiro - RJ');
+GO
 
-insert into TB_MESA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, QTD_CADEIRA, COD_RESTAURANTE, NUMERO)
-VALUES ( CURRENT_TIMESTAMP, null, 4, 1, 1);
+-- Carga na tabela tb_mesa
+INSERT INTO TB_MESA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, QTD_CADEIRA, COD_RESTAURANTE, NUMERO)
+VALUES (GETDATE(), NULL, 4, 1, 1);
 
-insert into TB_MESA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, QTD_CADEIRA, COD_RESTAURANTE, NUMERO)
-VALUES ( CURRENT_TIMESTAMP, null, 6, 1, 2);
+INSERT INTO TB_MESA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, QTD_CADEIRA, COD_RESTAURANTE, NUMERO)
+VALUES (GETDATE(), NULL, 6, 1, 2);
 
-insert into TB_MESA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, QTD_CADEIRA, COD_RESTAURANTE, NUMERO)
-VALUES ( CURRENT_TIMESTAMP, null, 8, 1, 3);
+INSERT INTO TB_MESA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, QTD_CADEIRA, COD_RESTAURANTE, NUMERO)
+VALUES (GETDATE(), NULL, 8, 1, 3);
+GO
 
-insert into TB_RESERVA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, USUARIO_COD, COMENTARIO, RESTAURANTE_COD, DATA_AGENDAMENTO,
+-- Carga na tabela tb_reserva
+INSERT INTO TB_RESERVA (DATA_DE_INCLUSAO, DATA_DE_ALTERACAO, USUARIO_COD, COMENTARIO, RESTAURANTE_COD, DATA_AGENDAMENTO,
                         MESA_COD, CHECKED_IN, CHECKED_OUT)
-VALUES (CURRENT_TIMESTAMP, null, 1, '01', 1, CURRENT_TIMESTAMP, 2, false, false)
+VALUES (GETDATE(), NULL, 1, '01', 1, GETDATE(), 2, 0, 0); -- Lembre-se: 0 para false
+GO
